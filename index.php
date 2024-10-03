6f285e45387ca8cfb9ce8c84412d06c48f253640
Androidfazal <163708486+Androidfazal@users.noreply.github.com>
2024-10-03 11:24:33 -0400

    added Index.php

---
+++ index.php
@@ -0,0 +1,275 @@
+<?php include('static/header.php');?>
+            <div class="row">
+                <div class="col-2">
+                    <h1>Stays up-to-date with ever-evolving fashion Trends, <br>A New Style</h1>
+                    <p></p>
+                    <a href="" class="btn"> Explore Now &#8594;</a>
+                </div>
+                <div class="col-2">
+                    <img src="images\models\image1.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---------featured---------->
+    <div class="categories">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3"> <img src="images\models\category-1.jpg"></div>
+                <div class="col-3"> <img src="images\models\category-2.jpg"></div>
+                <div class="col-3"> <img src="images\models\category-3.jpg"></div>
+            </div>
+        </div>
+    </div>
+    <!--------==========-PRODUCTS=======---------->
+    <div class="small-container">
+        <h2 class="title"> Products Featured </h2>
+        <div class="row">
+            <div class="col-4"><a href="product-details.html">
+                    <a href="product-details.html"><img src="images\models\product-1.jpg"></a>
+                    <a href="product-details.html">
+                        <h4>Red Printed T-shirt</h4>
+                    </a>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                    </div>
+
+                    <p>Rs 250.00</p>
+                </a>
+            </div>
+            <div class="col-4"><a href="product-details.html">
+                    <img src="images\models\product-2.jpg" alt="">
+                    <h4>Spark Shoe</h4>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                    </div>
+                    <p>Rs 400</p>
+            </div>
+            <div class="col-4"><a href="product-details.html">
+                    <img src="images\models\product-3.jpg" alt="">
+                    <h4>Men Cargo</h4>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                    </div>
+                    <p>Rs 350.00</p>
+                </a>
+            </div>
+            <div class="col-4"><a href="product-details.html">
+                    <img src="images\models\product-4.jpg" alt="">
+                    <h4>Black Printed T-shirt</h4>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                    </div>
+                    <p>Rs 250.00</p>
+                </a>
+            </div>
+            <h2 class="title">Latest Products</h2>
+            <div class="row">
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-5.jpg">
+                        <h4>white Spark shoe</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 550.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-6.jpg" alt="">
+                        <h4>Black Printed T-shirt</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 250.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-7.jpg" alt="">
+                        <h4>Ankle Socks for men & women</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  -----> <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 250.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-8.jpg" alt="">
+                        <h4>Clasic Watch for men</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 1100.00</p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-9.jpg">
+                        <h4>Clasic Watch for women</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 1250.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-10.jpg" alt="">
+                        <h4>Spark shoe </h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star-half" aria-hidden="true"></i>
+                            <i class="fa fa-star-o" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 2250.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-11.jpg" alt="">
+                        <h4>Grip fit slip ons for men</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  -----> <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                        <p>Rs 650.00</p>
+                    </a>
+                </div>
+                <div class="col-4"><a href="product-details.html">
+                        <img src="images\models\product-12.jpg" alt="">
+                        <h4>Men's Regular fit trackpants</h4>
+                        <div class="rating">
+                            <!---- star nahi  lag  pa rahe hai  ----->
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <p>Rs 250.00</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------OFFER----------------->
+    <div class="offer">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-2"> <img src="images\models\exclusive.png" class="offer-img">
+                </div>
+                <div class="col-2">
+                    <p>Excllusive Available on O-Shopping</p>
+                    <h1>Smart watch</h1> <small> Noise Icon Buzz 1.69" Display with Bluetooth Calling, Built-In Games,
+                        Voice Assistant Smartwatch (Deep Wine Strap, Regular)</small>
+                    <a href="" class="btn">Buy Now &#8594</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------------------------------Testimonial------------------------------------------------------>
+    <div class="testimonial">
+        <div class="small-cotainer">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p> Super build quality looks very premium.😍</p>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div> <img src="images\models\user-1.png">
+                    <h3> Katrina </h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p> The best super quality 😍😍😍😍😍</p>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <img src="images\models\user-2.png">
+                    <h3>harry</h3>
+                </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Very comfortable.. Love it.. Thank You SPARK 😊❤️ </p>
+                    <div class="rating">
+                        <!---- star nahi  lag  pa rahe hai  ----->
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div> <img src="images\models\user-3.png">
+                    <h3>Jakleen</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------------------------------BRANDS--------------------------------------------------------->
+    <div class="brands">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-5">
+                    <img src="images\brand\logo-godrej.png">
+                </div>
+                <div class="col-5">
+                    <img src="images\brand\logo-oppo.png">
+                </div>
+                <div class="col-5">
+                    <img src="images\brand\logo-coca-cola.png">
+                </div>
+                <div class="col-5">
+                    <img src="images\brand\gpaylogo.png.png  ">
+                </div>
+                <div class="col-5">
+                    <img src="images\brand\logo-white.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include('static/footer.php');?>
